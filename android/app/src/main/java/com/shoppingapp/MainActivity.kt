1cfad7625120520de6a2e0deb4c1b1ca20cbe599
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-07-25 16:15:25 +0700

    Remove bootsplash in android

--- android/app/src/main/java/com/shoppingapp/MainActivity.kt
+++ android/app/src/main/java/com/shoppingapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.shoppingapp
 
 import android.os.Bundle;
-import com.zoontek.rnbootsplash.RNBootSplash
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -9,8 +8,7 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
  override fun onCreate(savedInstanceState: Bundle?) {
-    RNBootSplash.init(this, R.style.BootTheme) // ⬅️ initialize the splash screen
-    super.onCreate(savedInstanceState) // super.onCreate(null) with react-native-screens
+    super.onCreate(null) // super.onCreate(null) with react-native-screens
   }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
