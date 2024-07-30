7fdb13fc08fa3c5524acbe2e3b20750666175509
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-07-30 13:49:25 +0700

    Config firebase for IOS

--- ios/Shara/AppDelegate.mm
+++ ios/Shara/AppDelegate.mm
@@ -1,4 +1,6 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
+
 
 
 #import <React/RCTBundleURLProvider.h>
@@ -10,11 +12,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"ShoppingApp";
+  self.moduleName = @"Shara";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
+  [FIRApp configure];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
